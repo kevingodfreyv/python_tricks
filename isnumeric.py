e7578dc2f78b0dcf04216723bc0f074e6e85a325
kevingodfreyv <kevingodfrey014@gmail.com>
2026-01-01 21:29:40 -0600

    isnumeric() function

---
+++ isnumeric.py
@@ -0,0 +1,5 @@
+
+# str.isnumeric(): Returns True if all characters in the string are numeric characters (including Unicode fractions and superscripts), and False if it contains symbols, decimals, or minus signs.
+print("1".isnumeric())
+print("12345".isnumeric())
+print("12.34".isnumeric())  
